8260f8e7bb2054ce71edea81ec9082b03c96519e
Danyor <dubashit@bk.ru>
2020-01-31 21:55:40 +0500

    Updates

---
+++ precache-manifest.5a83a11c1cd4fff8abce84c4c68b5705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb84674a6512e8376b108aa92bae9ec",
+    "url": "/HackerNews/index.html"
+  },
+  {
+    "revision": "ee6b22f7d43243a51656",
+    "url": "/HackerNews/static/css/main.a18bf36e.chunk.css"
+  },
+  {
+    "revision": "d769d291b794cb75714f",
+    "url": "/HackerNews/static/js/2.41bb3c67.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/HackerNews/static/js/2.41bb3c67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee6b22f7d43243a51656",
+    "url": "/HackerNews/static/js/main.9c4e0795.chunk.js"
+  },
+  {
+    "revision": "f8692643cd5dfd9dc5ff",
+    "url": "/HackerNews/static/js/runtime-main.73f5b31b.js"
+  }
+]);
